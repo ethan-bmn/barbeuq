f6613a23c958c96c0a24342eefaf1078c92daaac
Ethan <ethan.bmn@hotmail.com>
2026-01-26 23:15:51 +0100

    Feat: Add production environment specific features

---
+++ app/components/gamemode-link.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+const props = defineProps({
+    to: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    disableOnProd: {
+        type: Boolean,
+        required: false,
+        default: true,
+    },
+})
+
+const isProd = import.meta.env.VITE_ENV === 'production'
+
+const router = useRouter()
+
+function navigateTo(to: string) {
+    if (!(isProd && props.disableOnProd)) {
+        router.push(to)
+    }
+}
+</script>
+
+<template>
+    <a
+        :class="`link-btn ${isProd && disableOnProd ? 'disabled' : ''}`"
+        @click="navigateTo(to)"
+    >
+        <div class="inner-btn grid grid-cols-2 gap-6 relative">
+            <NuxtImg
+                :src="img"
+                class="my-auto link-img"
+            />
+            <div>
+                <h1>
+                    {{ title }}
+                </h1>
+                <p>
+                    {{ description }}
+                </p>
+            </div>
+            <div
+                v-if="isProd && disableOnProd"
+                class="coming-soon absolute top-1/4 text-white"
+            >
+                Bientôt disponible !
+            </div>
+        </div>
+    </a>
+</template>
+
+<style lang="scss">
+@use "sass:color";
+.link-btn {
+    display: grid;
+    background-color: $secondary;
+    transition: ease-in-out 0.4s;
+    z-index: 2;
+    text-align: center;
+    width: 80%;
+    margin: 0 auto;
+
+    font-family: "Belanosima", sans-serif;
+    font-style: normal;
+    font-weight: 600;
+
+    color: white;
+    border-radius: var(--radius-xl);
+    padding: calc(var(--spacing) * 3);
+
+    .inner-btn {
+        grid-template-columns: 20% auto;
+    }
+
+    h1 {
+        font-size: var(--text-lg);
+        text-align: start;
+        line-height: 25px;
+    }
+    p {
+        margin-top: var(--spacing);
+        font-size: var(--text-xs);
+        text-align: start;
+    }
+}
+
+.disabled {
+    background-color: color.adjust($secondary, $lightness: -13%);
+    color: #656565;
+
+    .inner-btn :not(.coming-soon) {
+        filter: blur(3px);
+    }
+
+    cursor: default;
+    .link-img {
+        opacity: 0.3;
+    }
+}
+
+.link-btn:not(.disabled):hover {
+    box-shadow: 0 15px 30px rgba(194, 88, 18, 0.5);
+    background-color: #3a3a3a;
+    transform: translateY(-5px);
+    cursor: pointer;
+}
+
+.link-img {
+    width: 46px;
+}
+
+.coming-soon {
+    width: calc(100% + calc(var(--spacing) * 6)); left: calc(var(--spacing) * -3);
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-const env = process.env.NODE_ENV
-
-console.log("env", env)
-</script>
-
 <template>
     <div class="flex justify-center w-full">
         <div
@@ -11,110 +5,26 @@ console.log("env", env)
             style="scrollbar-width: none;"
         >
             <div class="grid grid-rows-1 gap-5 w-full">
-                <NuxtLink
+                <GamemodeLink
+                    title="Classique"
+                    img="img/sausage.png"
+                    description="Cartes à boire tranquilles, pour bien commencer."
                     to="/setup?gamemode=classic"
-                    class="link-btn"
-                >
-                    <div class="inner-btn grid grid-cols-2 gap-6">
-                        <NuxtImg
-                            src="img/sausage.png"
-                            class="my-auto link-img"
-                        />
-                        <div>
-                            <h1>
-                                Classique
-                            </h1>
-                            <p>
-                                Cartes à boire tranquilles, pour bien commencer.
-                            </p>
-                        </div>
-                    </div>
-                </NuxtLink>
-                <NuxtLink
+                    :disable-on-prod="false"
+                />
+                <GamemodeLink
+                    title="Action ou vérité"
+                    img="img/truth-or-dare.png"
+                    description="Des questions et des actions"
                     to="/setup?gamemode=truth-or-dare"
-                    class="link-btn"
-                >
-                    <div class="inner-btn grid grid-cols-2 gap-6">
-                        <NuxtImg
-                            src="img/truth-or-dare.png"
-                            class="my-auto link-img"
-                        />
-                        <div>
-                            <h1>
-                                Action ou vérité
-                            </h1>
-                            <p>
-                                Des questions ou des actions.
-                            </p>
-                        </div>
-                    </div>
-                </NuxtLink>
-                <NuxtLink
+                />
+                <GamemodeLink
+                    title="Autobahn"
+                    img="img/autoroute.svg"
+                    description="La meilleure défense, c'est l'attaque."
                     to="/setup?gamemode=autobahn"
-                    class="link-btn belanosima text-5xl px-10 py-4 font-bold text-white rounded-lg"
-                >
-                    <div class="inner-btn grid grid-cols-2 gap-6">
-                        <NuxtImg
-                            src="img/autoroute.svg"
-                            class="my-auto link-img"
-                        />
-                        <div>
-                            <h1>
-                                Autobahn
-                            </h1>
-                            <p>
-                                La meilleure défense, c'est l'attaque.
-                            </p>
-                        </div>
-                    </div>
-                </NuxtLink>
+                />
             </div>
         </div>
     </div>
 </template>
-
-<style lang="scss">
-@use "sass:color";
-.link-btn {
-    display: grid;
-    background-color: $secondary;
-    transition: ease-in-out 0.4s;
-    z-index: 2;
-    text-align: center;
-    width: 80%;
-    margin: 0 auto;
-
-    font-family: "Belanosima", sans-serif;
-    font-style: normal;
-    font-weight: 600;
-
-    color: white;
-    border-radius: var(--radius-xl);
-    padding: calc(var(--spacing) * 3);
-
-    .inner-btn {
-        grid-template-columns: 20% auto;
-    }
-
-    h1 {
-        font-size: var(--text-lg);
-        text-align: start;
-        line-height: 25px;
-    }
-    p {
-        margin-top: var(--spacing);
-        font-size: var(--text-xs);
-        text-align: start;
-    }
-}
-
-.link-btn:hover {
-    box-shadow: 0 15px 30px rgba(194, 88, 18, 0.5);
-    background-color: #3a3a3a;
-    transform: translateY(-5px);
-}
-
-.link-img {
-    width: 46px;
-}
-</style>
