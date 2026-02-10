2d63512b72d54912754ff0e04eb0830abbec6ce8
Ethan <ethan.bmn@hotmail.com>
2026-02-10 22:50:13 +0100

    Chore: Add Tauri related files

---
+++ src-tauri/gen/android/app/src/main/java/com/barbeuq/stlr/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.barbeuq.stlr
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
