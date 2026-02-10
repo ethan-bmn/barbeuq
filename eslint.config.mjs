44f5da81e55766f1b0aec4f62c5a0605fc7eef1d
Ethan <ethan.bmn@hotmail.com>
2026-02-10 22:48:55 +0100

    Dev: Improve ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,15 @@ export default withNuxt(
         rules: {
             '@stylistic/brace-style': 'off',
             'vue/no-v-html': 'off',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'warn', // or 'error'
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
         },
     },
 )
